c69eb2b855693920cf30197271bfb057c4d4422a
matthewyip1511 <e0938566@u.nus.ed>
2024-10-26 13:48:09 +0800

    Set up profile page

--- src/app/humans-of-descholars/matthew-yip/page.tsx
+++ src/app/humans-of-descholars/matthew-yip/page.tsx
@@ -1,17 +1,16 @@
-import Image from "next/image";
+import Profile from '../components/Profile';
 
-export default function Page() {
-    return  (
-        <div>
-        <div>Matthew Yip (profile page)</div>
-        <Image
-        className="dark:invert"
-        src="/images/Matthew-Yip.jpg"
-        alt="Matthew Yip"
-        width={300}
-        height={300}
-        priority
-    />
-        </div>
+export default function ProfilePage() {
+    return (
+        <Profile
+            name="Matthew Yip"
+            academicYear="AY23/24"
+            course="Mechanical Engineering"
+            introduction="Hi there! Matthew here, Y3 ME."
+            interests="My interest particularly lies in F1 and 3D printing."
+            hobbies={['Running', 'Reading', 'Volunteering']}
+            imageUrl="/images/Matthew-Yip.jpg"
+            linkedInUrl="https://nusmods.com"
+        />
     );
 }
