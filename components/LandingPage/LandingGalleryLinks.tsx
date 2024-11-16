df126f90ae45b3b498d3a67a06c68a46495d420f
yeo-menghan <yeomenghan1989@gmail.com>
2024-11-16 23:25:24 +0800

    Add LandingPage - LandingGalleryLinks

---
+++ components/LandingPage/LandingGalleryLinks.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { Box, Grid, Typography } from "@mui/material";
+
+interface GalleryItem {
+  title: string;
+  image: string;
+  link: string;
+}
+
+interface LandingGalleryLinksProps {
+  galleryItems: GalleryItem[]; // Array of gallery items
+}
+
+const LandingGalleryLinks: React.FC<LandingGalleryLinksProps> = ({
+  galleryItems,
+}) => {
+  return (
+    <Box
+      sx={{
+        margin: "0 auto",
+        padding: "2rem 1rem",
+      }}
+    >
+      <Grid container spacing={4}>
+        {galleryItems.map((item, index) => (
+          <Grid item xs={12} sm={6} key={index}>
+            <Box
+              component="a"
+              href={item.link}
+              sx={{
+                display: "block",
+                textDecoration: "none",
+                textAlign: "center",
+                position: "relative",
+                overflow: "hidden",
+                borderRadius: "8px",
+                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+              }}
+            >
+              <img
+                src={item.image}
+                alt={item.title}
+                style={{
+                  width: "100%",
+                  height: "auto",
+                  borderRadius: "8px",
+                }}
+              />
+              <Typography
+                variant="h3"
+                sx={{
+                  position: "absolute",
+                  bottom: "40%",
+                  left: "50%",
+                  transform: "translateX(-50%)",
+                  color: "white",
+                  fontWeight: "bold",
+                  textShadow: "0px 2px 4px rgba(0, 0, 0, 0.5)",
+                  backgroundColor: "rgba(0, 0, 0, 0.4)",
+                  padding: "0.5rem 1rem",
+                  borderRadius: "4px",
+                }}
+              >
+                {item.title}
+              </Typography>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+export default LandingGalleryLinks;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
-import FullPageImage from "../../components/FullPageImage";
+import React from "react";
 import LandingImage from "../../components/LandingPage/LandingImage";
 import LandingDescription from "../../components/LandingPage/LandingDescription";
+import LandingGalleryLinks from "../../components/LandingPage/LandingGalleryLinks";
+
+const galleryItems = [
+  {
+    title: "About Us",
+    image: "/nusescholars/images/orientation2024.jpg",
+    link: "/about-us",
+  },
+  {
+    title: "Humans of DE-Scholars",
+    image: "/nusescholars/images/orientation2024.jpg",
+    link: "/humans",
+  },
+];
 
 export default function Page() {
   return (
@@ -12,6 +26,7 @@ export default function Page() {
       <LandingDescription
         text="Welcome to nusdescholars.com! This is an informal website run by the DE-Scholars Student Committee. Through this website, we hope to showcase what goes on in the life of a DE-Scholar. We also hope to provide a wide variety of information on the programme, from the academic aspects of the programme to the administrative aspects of applying to the programme!"
       />
+      <LandingGalleryLinks galleryItems={galleryItems} />
     </>
   );
 }
