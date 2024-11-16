51dd17baf22843ee3892ffe0883c6fcf2537b683
yeo-menghan <yeomenghan1989@gmail.com>
2024-11-16 23:18:37 +0800

    Add LandingPage - LandingDescription component

---
+++ components/LandingPage/LandingDescription.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+interface LandingDescriptionProps {
+  text: string; // Text to display in the description
+}
+
+const LandingDescription: React.FC<LandingDescriptionProps> = ({ text }) => {
+  return (
+    <Box
+      sx={{
+        width: "70%",
+        margin: "0 auto",
+        padding: "2rem 1rem",
+        textAlign: "center",
+      }}
+    >
+      <Typography variant="body1" sx={{ fontSize: "1.2rem", lineHeight: "1.8" }}>
+        {text}
+      </Typography>
+    </Box>
+  );
+};
+
+export default LandingDescription;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import FullPageImage from "../../components/FullPageImage";
 import LandingImage from "../../components/LandingPage/LandingImage";
+import LandingDescription from "../../components/LandingPage/LandingDescription";
 
 export default function Page() {
   return (
-    <LandingImage
-      imageUrl="/nusescholars/images/orientation2024.jpg"
-      title="Welcome to the D-E scholars webpage!"
-    />
+    <>
+      <LandingImage
+        imageUrl="/nusescholars/images/orientation2024.jpg"
+        title="Welcome to the D-E scholars webpage!"
+      />
+      <LandingDescription
+        text="Welcome to nusdescholars.com! This is an informal website run by the DE-Scholars Student Committee. Through this website, we hope to showcase what goes on in the life of a DE-Scholar. We also hope to provide a wide variety of information on the programme, from the academic aspects of the programme to the administrative aspects of applying to the programme!"
+      />
+    </>
   );
 }
