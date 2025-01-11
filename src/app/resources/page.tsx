5510abffc3b3484b2841994ba91c9f6c44bb4f8a
yeo-menghan <yeomenghan1989@gmail.com>
2025-01-11 19:56:55 +0800

    Add intermediate page before external resource page

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,14 +1,43 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PageTemplate from "../../../components/PageTemplate";
+import HeroSection from "../../../components/HeroSection";
+
 
 const Resources: React.FC = () => {
+  const [countdown, setCountdown] = useState(5);
+  const [width, setWidth] = useState(100); // Initial width for the loading bar
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCountdown((prev) => {
+        if (prev === 1) {
+          clearInterval(timer);
+          window.location.href = "https://sites.google.com/view/nus-e-scholars/home?authuser=0";
+          return 0;
+        }
+        setWidth((prevWidth) => prevWidth - 20); // Decrease width for each second
+        return prev - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <PageTemplate>
-      <div style={{ textAlign: "center", padding: "50px" }}>
-        <h1>Resources</h1>
-        <p>This is the Resources page placeholder. Add your content here.</p>
+      <HeroSection
+        title="Resources"
+      />
+      <div style={{ textAlign: "center", padding: "50px", fontSize: "24px" }}>
+        <p>This page will redirect you to the Engineering Scholars Programme in {countdown} seconds.</p>
+        <p>
+          If you want to go there immediately,{" "}
+          <a href="https://sites.google.com/view/nus-e-scholars/home?authuser=0" style={{ textDecoration: "underline" }}>
+            click here
+          </a>.
+        </p>
       </div>
     </PageTemplate>
   );
